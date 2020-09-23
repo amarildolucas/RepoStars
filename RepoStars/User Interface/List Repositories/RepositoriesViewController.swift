7ed27841b5c67e7efa9cc01f017280afbb36be54
Amarildo Lucas <amarildo.lucas@orama.com.br>
2020-09-23 17:35:08 -0300

    removed unnecessary files

--- RepoStars/User Interface/List Repositories/RepositoriesViewController.swift
+++ RepoStars/User Interface/List Repositories/RepositoriesViewController.swift
@@ -7,13 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view.
-	}
-
-
+// MARK: - RepositoriesViewController
+class RepositoriesViewController: UIViewController {
 }
 
+// MARK: - Lifecycle
+extension RepositoriesViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
